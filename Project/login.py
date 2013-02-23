55b8f37933816d74060246c3732f6b98caeae858
kaosx5s <kaos_x5s@yahoo.com>
2013-02-23 06:42:40 -0800

    Adds login.py to Project folder.

---
+++ Project/login.py
@@ -0,0 +1,13 @@
+#User Module
+#uses the GAE users library for our accounts module
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+
+class LoginPage(webapp.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		if user:
+			self.redirect('/')
+		else:
+			self.redirect(users.create_login_url("/"))
