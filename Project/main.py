4f8f57238fd6c75d96adc1b88b87d1f51a1d8ac8
Chad <chad.wyszynski@csu.fullerton.edu>
2013-02-23 09:28:26 -0800

    Added nav bar to top

--- Project/main.py
+++ Project/main.py
@@ -14,15 +14,18 @@ from google.appengine.api import users
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 j_env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
 
+class MockUser():
+    def __init__(self):
+        self.nickname = "Bob"
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = j_env.get_template(config.main_page)
-	self.response.write('''
-		<a href="/article">Post new article</a>
-	''')
-	articles = datastore.Get_Articles()
-        self.response.write(template.render(title=config.main_title,data = articles))
+        articles = datastore.Get_Articles()
+
+        self.response.write(template.render(title=config.main_title, 
+                                            data=articles, 
+                                            user=MockUser()))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
