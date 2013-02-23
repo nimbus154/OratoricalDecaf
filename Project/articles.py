131e08c4dde8b20f89d5fb44b1a49e45f94ceafe
Chad <chad.wyszynski@csu.fullerton.edu>
2013-02-23 10:20:12 -0800

    Styled post article page

--- Project/articles.py
+++ Project/articles.py
@@ -8,42 +8,36 @@
 import datastore
 import webapp2
 import cgi
+import config
 from google.appengine.api import users
 
-form = """
-<html>
-	<body>
-		<form method="post">
-			<div><h1>Post Page</h1></div>
-			<div>Title:</div>
-			<div><textarea name="link_title" rows="2" cols="60"></textarea></div>
-			<div>Location/URL:<br></div>
-			<div><textarea name="link_url" rows="2" cols="60"></textarea></div>
-			<div><input type="submit" value="Post"></div>
-		</form>
-	</body>
-</html>
-"""
 
 def escape_html(s):
 		return cgi.escape(s, quote = True)
 		
 class PostPage(webapp2.RequestHandler):
-	def write_form(self, error="", title="", url=""):
-		self.response.out.write(form %{"error": error,
-                                       "link_title": escape_html(title),
-                                       "link_url": escape_html(url)})
-	def get(self):
-		#We should check to see if the user is logged in here instead of after our POST.
-		if users.get_current_user():
+    def write_form(self, error="", title="", url=""):
+        template=config.templates.get_template(config.article_post_page)
+        self.response.out.write(template.render(
+                                            { 
+                                                "user": users.get_current_user(),
+                                                "error": error,
+                                                "title": title,
+                                                "url": url
+                                            }))
+
+    def get(self):
+        # We should check to see if the user is logged in here 
+        # instead of after our POST.
+        if users.get_current_user():
 		    self.write_form()
-		else:
+        else:
 		    self.redirect(users.create_login_url(self.request.uri))
 
-	def post(self):
-		user = users.get_current_user() 
-		user_link_url = self.request.get('link_url')
-		user_link_title = self.request.get('link_title')
-		user_name = user.nickname()
-		datastore.Post_Article(user_link_url,user_link_title,user_name)
-		self.redirect("/")
+    def post(self):
+        user = users.get_current_user() 
+        user_link_url = self.request.get('link_url')
+        user_link_title = self.request.get('link_title')
+        user_name = user.nickname()
+        datastore.Post_Article(user_link_url,user_link_title,user_name)
+        self.redirect("/")
--- Project/config.py
+++ Project/config.py
@@ -1,4 +1,12 @@
 # This file contains hardcoded strings and values
+import jinja2
+import os
+
+# jinja2 file loading copied from 
+# https://github.com/fRuiApps/cpfthw/blob/master/webapp2/views.py
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+templates = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR))
 
 main_page="article_list.html"
+article_post_page="post_article.html"
 main_title="Oratorical Decaf"
